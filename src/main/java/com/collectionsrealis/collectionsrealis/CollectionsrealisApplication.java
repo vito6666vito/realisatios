f4119280571bc00391bc6c06722481be0436fce6
vito6666vito <vito6666vito@gmail.com>
2023-05-08 21:44:12 +0300

    realisation hw

---
+++ src/main/java/com/collectionsrealis/collectionsrealis/CollectionsrealisApplication.java
@@ -0,0 +1,13 @@
+package com.collectionsrealis.collectionsrealis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CollectionsrealisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CollectionsrealisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/collectionsrealis/collectionsrealis/CollectionsrealisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.collectionsrealis.collectionsrealis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CollectionsrealisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
